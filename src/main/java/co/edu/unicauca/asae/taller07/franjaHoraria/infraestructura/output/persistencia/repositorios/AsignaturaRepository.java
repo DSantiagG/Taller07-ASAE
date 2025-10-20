717b368933c0851fafd32d494911a98eda5faaaf
David Santiago Giron Muñoz <98716905+DSantiagG@users.noreply.github.com>
2025-10-20 11:31:55 -0500

    fix Rest Curso

--- src/main/java/co/edu/unicauca/asae/taller07/franjaHoraria/infraestructura/output/persistencia/repositorios/AsignaturaRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unicauca.asae.taller07.franjaHoraria.infraestructura.output.persistencia.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.unicauca.asae.taller07.franjaHoraria.infraestructura.output.persistencia.entidades.AsignaturaEntity;
-
-public interface AsignaturaRepository extends JpaRepository<AsignaturaEntity, Integer>{
-
-}
